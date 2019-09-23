ddf757d7bd110f8fa947162aca129242c1e8e04b
Christopher Whitehead <cawhitehead@gmail.com>
2019-09-22 21:37:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/webrtctest/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.webrtctest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.core.app.ActivityCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Log
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var webView: WebView
+    private val url = "https://c-a-w-webrtc.herokuapp.com"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        WebView.setWebContentsDebuggingEnabled(true)
+        webView = findViewById(R.id.main_webview)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest?) {
+                if (request == null) {
+                    Log.i("WEB_CHROME_CLIENT", "no request returned")
+                } else {
+                    Log.i("WEB_CHROME_CLIENT", request.toString())
+                    request.grant(request.resources)
+                }
+            }
+        }
+
+
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.CAMERA)) {
+            // Show an explanation to the user *asynchronously* -- don't block
+            // this thread waiting for the user's response! After the user
+            // sees the explanation, try again to request the permission.
+        } else {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO
+                ),
+                1
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            1 -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+
+                    webView.loadUrl(url)
+                } else {
+                    Log.d("PERMISSION", "permission denied")
+                }
+                return
+            }
+        }
+    }
+}
